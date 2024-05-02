6443d2aa6b506085618a1c25c6f3916a4b044b2f
Brianna Tran <briannatran45@gmail.com>
2024-05-02 15:41:41 -0700

    created getUser fn

--- auth.ts
+++ auth.ts
@@ -3,6 +3,24 @@ import { authConfig } from "./auth.config";
 import Credentials from 'next-auth/providers/credentials';
 import { z } from "zod";
 import credentials from "next-auth/providers/credentials";
+import { sql } from "@vercel/postgres";
+import type { User } from "@/app/lib/definitions";
+import bcrypt from 'bcrypt';
+
+async function getUser(email: string): Promise<User | undefined> {
+  try {
+    const user = await sql<User>`
+      SELECT *
+      FROM users
+      WHERE email = ${email}
+    `;
+    return user.rows[0];
+  }
+  catch (err) {
+    console.error('Failed to fetch user:', err);
+    throw new Error('Failed to fetch user.');
+  }
+}
 
 export const { auth, signIn, signOut } = NextAuth({
   ...authConfig,
@@ -15,6 +33,16 @@ export const { auth, signIn, signOut } = NextAuth({
             password: z.string().min(6)
           })
           .safeParse(credentials);
+
+        if (parsedCredentials.success) {
+          const { email, password } = parsedCredentials.data;
+          const user = await getUser(email);
+          if (!user) {
+            return null;
+          }
+        }
+
+        return null;
       },
     })
   ]
